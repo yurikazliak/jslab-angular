11138b7350c986460fda56d0357e8c2e1ccc69cc
Yuri Kazlaik <guinnesswud1@gmail.com>
2019-09-14 19:07:24 +0300

    feat: implement basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { CardListComponent } from './card-list/card-list.component';
+import { CardItemComponent } from './card-item/card-item.component';
+import { BoardComponent } from './board/board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ToolbarComponent,
+    CardListComponent,
+    CardItemComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule
